a3511e4f0509e35fc5c08dad4b9d1ca144bbd260
jxa <julianalbert0012@gmail.com>
2024-08-11 12:41:14 -0700

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,28 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBl5gLLQG84kGXHm5_XznAdSjEH6eYgvyw",
-  authDomain: "hagel-ae59a.firebaseapp.com",
-  projectId: "hagel-ae59a",
-  storageBucket: "hagel-ae59a.appspot.com",
-  messagingSenderId: "422349601687",
-  appId: "1:422349601687:web:ef3b8684ecf60814f8e2d2",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
 
 // Initialize Firestore and get a reference to the service
 export const firestore = getFirestore(app);
